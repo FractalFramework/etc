4cd371b7d44dd777e1907af9c67756d7b67a929a
fractalframework <dav8119@gmail.com>
2023-08-17 01:39:01 +0200

    230817

---
+++ dav/db.php
@@ -0,0 +1,21 @@
+<?php
+class db{
+
+    static function db($b){
+        $r=json::call('cnfg/db');
+        return $r[$b]??[];
+    }
+
+    static function install($b){
+        sql::create($b,self::db($b),1);
+    }
+
+    static function install_all($b){
+        $r=json::call('cnfg/db');
+        foreach($r as $k=>$v)
+            sql::create($r,$v,1);
+    }
+
+    static function call(){
+    }
+}
